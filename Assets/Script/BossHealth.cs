527385711690767bb5b2a4daf39b4c56bf208722
Sam <neiamenase@gmail.com>
2018-03-31 18:22:47 +0800

    rephase boss script

--- Assets/Script/BossHealth.cs
+++ Assets/Script/BossHealth.cs
@@ -7,15 +7,17 @@ public class BossHealth : MonoBehaviour {
 	public float initialHealth = 100f;
 	public float currentHealth = 0;
 	Animator animator;
-	public float dieCompleteTime = 15f;
+	public float dieCompleteTime = 10f;
 	private float timer;
 	public bool isDead = false;
+	Boss bossScript;
 	//private ParticleSystem WalkerParticleSystem;
 
 	void Start () {
 		currentHealth = initialHealth;
 		animator = GetComponent<Animator>();
 		timer = 0;
+		bossScript = GetComponent<Boss> ();
 
 //		WalkerParticleSystem = GameObject.Find("Walker").GetComponent<ParticleSystem>();
 //		WalkerParticleSystem.Stop();
@@ -32,19 +34,22 @@ public class BossHealth : MonoBehaviour {
 	public void TakeDamage(float amount)
 	{
 		//WalkerParticleSystem.Play();
-		Debug.Log("Current HP:" + amount);
-		currentHealth -= amount;
-		if(currentHealth <= 0f)
-		{
-			isDead = true;
+		if (bossScript.start) {
+			Debug.Log ("Current HP:" + amount);
+			currentHealth -= amount;
+			if (currentHealth < 100 && currentHealth % 10 == 0) {
+				animator.SetTrigger ("getHit");
+				bossScript.stopCompleteTime = 1f;
+			}
+			if (currentHealth <= 0f) {
+				animator.SetBool ("isDead", true);
+				isDead = true;
+			} 
 		}
 	}
 
 	public void Die()
 	{
-		if (timer == 0) {
-			animator.SetBool ("gethit", true);
-		}
 		if (timer >= dieCompleteTime) {
 			Destroy (gameObject);
 			timer = 0;
