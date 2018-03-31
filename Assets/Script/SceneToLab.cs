3862b95460a96aa26913c17bc0f0c58417ff0f13
Ryan <iwlun0212@gmail.com>
2018-03-31 16:30:37 +0800

    highway

---
+++ Assets/Script/SceneToLab.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+
+public class SceneToLab : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+		
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.tag == "Player")
+		{
+			SceneManager.LoadScene ("Lab");
+
+		}
+	}
+
+}
