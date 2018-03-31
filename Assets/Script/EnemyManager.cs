77b7d1e0afda4b768d90d5dcaa7ff6d140463e77
cycodeho <hocyalbert@gmail.com>
2018-03-31 19:14:48 +0800

    walker respawn

---
+++ Assets/Script/EnemyManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour {
+
+    public GameObject enemy;                // The enemy prefab to be spawned.
+    public float spawnTime = 10f;            // How long between each spawn.
+    public Transform[] spawnPoints;         // An array of the spawn points this enemy can spawn from.
+
+
+    void Start () {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+	
+	// Update is called once per frame
+	void Spawn() {
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+        // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
+        Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+    }
+}
