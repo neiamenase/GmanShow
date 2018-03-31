dd36ddc4b0501ff97e4e456183ce1fc16b68a80c
Ryan <iwlun0212@gmail.com>
2018-03-31 19:36:22 +0800

    lab2

---
+++ Assets/Script/FlickeringLight.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickeringLight : MonoBehaviour {
+
+	Light myLight;
+	public float minWaitTime;
+	public float maxWaitTime;
+	// Use this for initialization
+	void Start () {
+		myLight = GetComponent<Light> ();
+	}
+
+	IEnumerator Flashing(){
+		while(true){
+			yield return new WaitForSeconds (Random.Range(minWaitTime
+				, maxWaitTime));
+			myLight.enabled = !myLight.enabled;
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
