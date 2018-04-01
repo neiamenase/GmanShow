ec528e261bed5a51ce39dc667846d545276d44f2
cuhkcse <chrisyuen.cuhk@gmail.com>
2018-04-01 09:56:41 +0800

    Add Dark Lab with Lake

---
+++ Assets/Script/ShowNextScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ShowNextScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other) {
+		SceneManager.LoadScene("Dark Lab");
+	}
+}
