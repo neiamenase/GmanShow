2fc61ddb3235f14c88c49ee150c7976183609ae3
wingytsui8 <tsuiwingyan@gmail.com>
2018-04-02 21:38:15 +0800

    add electronicDoor in lighting room

---
+++ Assets/Script/ElectronicDoor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ElectronicDoor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnCollisionEnter(Collision collision) {
+		print (collision.gameObject.name);
+		if (collision.gameObject.name == "Player_2" ) {
+			GameObject.FindGameObjectWithTag("Dialogue").GetComponentsInChildren<Text>()[0].text = "It is an electronic door. \nPlease restart power generator.";
+
+		}
+	}
+}
